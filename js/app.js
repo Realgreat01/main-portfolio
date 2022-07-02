ff83ce4ed2a9e3ee82ba760af1c96b697c9f345a
Realgreat01 <samsonrealgreat@gmail.com>
2022-07-02 16:27:47 +0100

    dark mode

--- js/app.js
+++ js/app.js
@@ -36,23 +36,13 @@ clearInterval(projectAdd, 3000)
 const darkMode = ()=>{
     body.style.setProperty('--primary', '#050000');
     body.style.setProperty('--secondary', '#ff0077');
-    body.style.setProperty('--tertiary', '#ff0077');
-    body.style.setProperty('--dark', '#f9f9fc');
-    body.style.setProperty('--background', '#fff');
-  
-    // images.forEach((image)=>{
-    //   image.style.filter = "none";
+    body.style.setProperty('--tertiary', '#0126f7');
+    body.style.setProperty('--dark', '#fff');
+    body.style.setProperty('--background', 'linear-gradient(140deg, #fff, #dcfbfc)');
+    body.style.setProperty('--box', '#6bb5fa');
+    body.style.setProperty('--nav', '#b9f4f5');
 
-    //   image.addEventListener("mouseover", ()=>{
-    //     image.style.filter="none"
-    //   });
-
-    //   image.addEventListener("mouseout", ()=>{
-    //     image.style.filter="none"
-    //   });
-    // });
-
-    light.style.visibility="visible";
+    // light.style.visibility="visible";
     light.style.visibility="hidden";
     dark.style.visibility="visible";
 
@@ -65,22 +55,14 @@ const darkMode = ()=>{
 
   const lightMode = ()=>{;
     body.style.setProperty('--primary', '#fff');
-    body.style.setProperty('--secondary', '#2929f8');
-    body.style.setProperty('--tertiary', '#ff0077');
-    body.style.setProperty('--dark', '#2d2d30');
-    body.style.setProperty('--background', '#050000');
-    
-    //  images.forEach((image)=>{
-      // image.style.filter = "grayscale(100%)";
-      
-      //   image.addEventListener("mouseover", ()=>{
-        //     image.style.filter="none"
-        //   });
-        //   image.addEventListener("mouseout", ()=>{
-          //     image.style.filter="grayscale(100%)"
-    //   });
+    body.style.setProperty('--secondary', '#fa2388');
+    body.style.setProperty('--tertiary', '#7afa03');
+    body.style.setProperty('--dark', '#1a0129');
+    body.style.setProperty('--background', 'linear-gradient(105deg, #240127ee ,#0d0122 50%, #07010f)');
+    body.style.setProperty('--box', '#52606d');
+    body.style.setProperty('--nav', '#2b024d');
+ 
 
-    // });
     dark.style.visibility="hidden";
     light.style.visibility="visible";
 
@@ -90,21 +72,28 @@ const darkMode = ()=>{
 
     setInterval(projectAdd, 50);
   }
-//   navbar.style.scrollBehavior="smooth";
 
-//   let scrollval = 0;
-//       window.addEventListener('scroll', () => {
-//   if(scrollval > window.scrollY) {
-//     navbar.style.position="static"; 
-//     navbar.style.background="none";
-   
-//   } else {
-  //     navbar.style.position="fixed";
-  //     navbar.style.background="red";
-  //     navbar.style.transition="1s";
-  //   }
-  //   scrollval = window.scrollY;
-  // });
+  let scrollval = 900;
+  window.addEventListener('scroll', () => {
+    if(scrollval >  window.scrollY) {
+      navbar.style.position="static"; 
+      navbar.style.background="none";
+      console.log("scroll up")
+      
+    }else if(window.scrollY<=600){
+      console.log("scroll top")
+      navbar.style.background="none";
+      
+    } else {
+      console.log("scroll bottom")
+      navbar.style.position="fixed";
+          navbar.style.background="var(--nav)";
+          navbar.style.transition="1s";
+          navbar.style.top="10px";
+
+        }
+    scrollval=window.scrollY;
+  });
   
   const showMenu =()=>{
   if(mobileNav.style.height="0"){
@@ -120,9 +109,28 @@ const hideMenu =()=>{
   menuIcon.style.visibility="visible";
   cancelIcon.style.visibility="hidden";
 }
-
 menuIcon.addEventListener("click", showMenu)
 cancelIcon.addEventListener("click", hideMenu)
 
 mobileNav.addEventListener("mouseleave", hideMenu);
+
+const readMore = document.querySelectorAll(".read-more");
+const briefInfo = document.querySelectorAll(".brief-info");
+
+let valueBtn = true;
+    for(let i =0; i<briefInfo.length; i++){
+      readMore[i].addEventListener("click", ()=>{
+        if(valueBtn){
+          valueBtn=false;
+          briefInfo[i].textContent= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, at nam non distinctio magnam harum, repudiandae incidunt facere eaque illum dolore nisi soluta dolores, quidem cum necessitatibus velit deserunt similique aliquam nostrum. Quod nobis quae hic deserunt animi aperiam ipsum. Nulla expedita sunt quasi autem nihil ab magnam accusamus. Accusantium, laudantium? Voluptas accusantium delectus impedit natus hic aperiam magnam neque excepturi nihil ipsam accusamus quia tempore dicta eum voluptatibus voluptatem culpa odio reprehenderit laborum, minus facere? Itaque, impedit! Dolor dolorum quae odio aliquid dolores quidem, cum minima perferendis ducimus dicta deleniti soluta, itaque optio illum aspernatur vitae expedita iusto quis, natus aperiam laboriosam accusamus. Atque reiciendis sit fugiat fugit ab dolores. Voluptatibus ex cum pariatur deserunt asperiores rem eligendi saepe."
+          readMore[i].innerText ="Show Less ↗";
+          
+        }else{
+          valueBtn=true;
+          briefInfo[i].textContent= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quam in, totam sunt, ipsa quia tempore voluptates culpa iure distinctio ipsam odit ullam nisi sed earum itaque, repudiandae saepe illum odio impedit provident libero. Molestiae praesentium explicabo, perspiciatis dignissimos nam modi nesciunt eligendi maiores, labore, laborum fugit quos facilis voluptas."
+          readMore[i].innerText ="Read More ↗";
+            }
+      })
+    }
+
 
